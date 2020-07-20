65706641af11d6ae71dc6169ac9ec2c1ca89f961
IshanPradhan <ishan.pradhan007@gmail.com>
2020-07-20 13:01:45 +0530

    Updates

---
+++ precache-manifest.02697beb0214d3a30c4509d39feddb58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d999d79ba48a06fb695f30e1b26929",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcb5ced0cd50de4c1b24",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fda0fb1f7f71f49adbd1",
+    "url": "/static/css/main.fb8525e0.chunk.css"
+  },
+  {
+    "revision": "dcb5ced0cd50de4c1b24",
+    "url": "/static/js/2.bea7dee1.chunk.js"
+  },
+  {
+    "revision": "2c21478778d46668a7c75a32a167f767",
+    "url": "/static/js/2.bea7dee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fda0fb1f7f71f49adbd1",
+    "url": "/static/js/main.6658e4ea.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
