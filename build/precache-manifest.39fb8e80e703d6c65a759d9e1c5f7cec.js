15f92cf4dc08bcb5e458ccd0ba6f75265ecf6053
IshanPradhan <ishan.pradhan007@gmail.com>
2020-08-19 15:26:07 +0530

    package changes

---
+++ build/precache-manifest.39fb8e80e703d6c65a759d9e1c5f7cec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb361d948f9af4a8d337f907692a3bb6",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "13d88b417cf3ae43d6fa",
+    "url": "/portfolio-website/static/css/2.a34ee582.chunk.css"
+  },
+  {
+    "revision": "f82176bf17b806ea0188",
+    "url": "/portfolio-website/static/css/main.ac440ca9.chunk.css"
+  },
+  {
+    "revision": "13d88b417cf3ae43d6fa",
+    "url": "/portfolio-website/static/js/2.ab34b8f0.chunk.js"
+  },
+  {
+    "revision": "2c21478778d46668a7c75a32a167f767",
+    "url": "/portfolio-website/static/js/2.ab34b8f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f82176bf17b806ea0188",
+    "url": "/portfolio-website/static/js/main.d5c2931b.chunk.js"
+  },
+  {
+    "revision": "a77bb18274229563e654",
+    "url": "/portfolio-website/static/js/runtime-main.5816c056.js"
+  }
+]);
